82ffdeff07b6a521411cef10ff0a138491221957
Leroy Rosales <leroyrosales@gmail.com>
2021-12-11 12:21:42 -0600

    Adds navigation component

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,13 @@
 import React from 'react'
 import { useState } from 'react'
+import Navigation from './components/nav.component'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="bg-black h-screen flex">
-      <header className="container flex-col justify-items-center self-center mx-auto px-4 text-center text-white text-2xl">
-
-        <p>Hello Vite + React + Tailwind!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div className="h-screen flex">
+      <Navigation/>
     </div>
   )
 }
---
+++ src/components/nav.component.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function Navigation() {
+  return (
+    <>
+      <nav className="container mx-auto">
+          <ul>
+          <li className="mr-4 inline">Sneakers</li>
+          <li className="mr-4 inline">Collections</li>
+          <li className="mr-4 inline">Men</li>
+          <li className="mr-4 inline">Women</li>
+          <li className="mr-4 inline">About</li>
+          <li className="mr-4 inline">Contact</li>
+          </ul>
+      </nav>
+    </>
+  );
+}
